9975757ac90d5f298c87fdd1b807c3da18bc69b2
Daan van der Kallen <mail@daanvdk.com>
2018-07-12 10:28:58 +0200

    Add MIT license

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ BINDER_TAG = '1.2.0'
 setup(
     name='django-tally',
     packages=['django_tally'],
+    license='MIT',
     version='0.1.0',
     description='A package for easily tallying Django data.',
     author='Daan van der Kallen',
@@ -17,10 +18,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
